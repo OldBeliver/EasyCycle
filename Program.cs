4a58da46518b2a93a11346da6e3ed4596f59e6b5
OldBeliver <380506338004@yandex.com>
2022-09-27 12:55:29 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EasyCycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string voice;
+            int count;
+
+            Console.WriteLine($"Какие звуки издает животное, которе на \"ля\" начинается и \"гушка\" заканчивается? ");
+            voice = Console.ReadLine();
+
+            Console.Write($"сколько раз издать этот звук? ");
+            count = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write($"{voice} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
